31323325600b11dbf868edb33071fea6102a5124
Vianney-Gln <vgeloen@gmail.com>
2022-02-02 16:36:21 +0100

    routing ok fin du sprint1

--- checkpoint4/src/App.js
+++ checkpoint4/src/App.js
@@ -1,13 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header";
 import Home from "./components/home";
+import Post from "./components/post";
+import Login from "./components/Login";
 //import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/post" element={<Post />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
       {/* <Footer /> */}
     </div>
   );
--- checkpoint4/src/components/header.jsx
+++ checkpoint4/src/components/header.jsx
@@ -1,4 +1,5 @@
 import "../styles/header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -6,9 +7,16 @@ const Header = () => {
       <nav>
         <ul>
           <li>Logo</li>
-          <li>Accueil</li>
-          <li>Poster un Facts</li>
-          <li>Login</li>
+          <Link to="/">
+            <li>Accueil</li>
+          </Link>
+          <Link to="/post">
+            <li>Poster un Facts</li>
+          </Link>
+          <Link to="/login">
+            {" "}
+            <li>Login</li>
+          </Link>
         </ul>
       </nav>
     </div>
