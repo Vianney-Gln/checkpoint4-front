f37c69b07ebcfc6d279807777891b361b8d38bf3
Vianney-Gln <vgeloen@gmail.com>
2022-02-02 12:18:14 +0100

    mise en place des premiers composants header home footer avec un peu de style

---
+++ checkpoint4/src/components/header.jsx
@@ -0,0 +1,18 @@
+import "../styles/header.scss";
+
+const Header = () => {
+  return (
+    <div className="container-header">
+      <nav>
+        <ul>
+          <li>Logo</li>
+          <li>Accueil</li>
+          <li>Poster un Facts</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ checkpoint4/src/components/home.jsx
@@ -0,0 +1,14 @@
+import "../styles/home.scss";
+const Home = () => {
+  return (
+    <div className="container-home">
+      <p className="text-welcome">
+        Bienvenue dans le remake des Chuck Norris Facts! Un projet que j ai
+        toujours voulu réaliser pour le fun, et qui mettra en pratique toutes
+        les compétences apprises durant ces 5 mois de formation.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Home;
