cb3b09cbaaed7c717dd25d252b4354786e109798
Vianney-Gln <vgeloen@gmail.com>
2022-02-02 12:31:51 +0100

    mise en place du composant joke en statique avec son style

---
+++ checkpoint4/src/components/Joke.jsx
@@ -0,0 +1,13 @@
+import "../styles/joke.scss";
+const Joke = () => {
+  return (
+    <div className="container-joke">
+      <p>
+        Les samouraïs tuent des mouches avec leurs sabres... Chuck Norris, lui,
+        tue des samouraïs avec des mouches
+      </p>
+    </div>
+  );
+};
+
+export default Joke;
--- checkpoint4/src/components/home.jsx
+++ checkpoint4/src/components/home.jsx
@@ -1,12 +1,17 @@
 import "../styles/home.scss";
+import Joke from "./Joke";
 const Home = () => {
   return (
     <div className="container-home">
-      <p className="text-welcome">
-        Bienvenue dans le remake des Chuck Norris Facts! Un projet que j ai
-        toujours voulu réaliser pour le fun, et qui mettra en pratique toutes
-        les compétences apprises durant ces 5 mois de formation.{" "}
-      </p>
+      <div className="container-text-welcome">
+        <p className="text-welcome">
+          Bienvenue dans le remake des Chuck Norris Facts! Un projet que j ai
+          toujours voulu réaliser pour le fun, et qui mettra en pratique toutes
+          les compétences apprises durant ces 5 mois de formation.{" "}
+        </p>
+      </div>
+
+      <Joke />
     </div>
   );
 };
