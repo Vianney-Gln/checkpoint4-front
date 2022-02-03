01c77c334e1738bb40bfe4fbfba706791fef0668
Vianney-Gln <vgeloen@gmail.com>
2022-02-03 22:16:22 +0100

    suppression du Login

--- checkpoint4/src/App.js
+++ checkpoint4/src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 import Header from "./components/header";
 import Home from "./components/home";
 import Post from "./components/post";
-import Login from "./components/Login";
+
 //import Footer from "./components/footer";
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/post" element={<Post />} />
-        <Route exact path="/login" element={<Login />} />
       </Routes>
       {/* <Footer /> */}
     </div>
